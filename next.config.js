4365135e0366a01328e03479564ef21f4833ce96
John Gilbert <gilbert.johnt@gmail.com>
2023-10-01 15:38:36 -0400

    feat: initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/a-keen-eye" : undefined,
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
